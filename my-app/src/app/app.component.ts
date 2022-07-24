4febec256f61cc84e21d8741eb09cc40cbc6bc2b
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-07-24 19:41:31 +0100

    observables added and subject replaced eventemitter in main app

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  turner = true;
-  page(recieve: boolean) {
-    recieve? this.turner = true: this.turner = false;
-  }
+  
 
 }
